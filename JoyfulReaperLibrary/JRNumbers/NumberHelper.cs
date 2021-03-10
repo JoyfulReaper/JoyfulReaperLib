415dd8508cc248b067586608c7c90c5b060f814a
Kyle Givler <xvegan88x@gmail.com>
2021-03-09 21:15:06 -0500

    Method to get the number of digits in an int

---
+++ JoyfulReaperLibrary/JRNumbers/NumberHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JoyfulReaperLib.JRNumbers
+{
+    public static class NumberHelper
+    {
+        /// <summary>
+        /// Get the number of digits for a given number
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns>The number of digts in the given number</returns>
+        public static int NumberOfDigits(int number)
+        {
+            int numDigits = 1;
+            while ((number /= 10) != 0)
+            {
+                numDigits++;
+            }
+
+            return numDigits;
+        }
+    }
+}
