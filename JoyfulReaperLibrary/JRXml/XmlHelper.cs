ce162e6f072f293a52ae86268f7baaa319268e25
Kyle Givler <xvegan88x@gmail.com>
2021-04-14 21:28:26 -0400

    Generic method to de-serialize XML

---
+++ JoyfulReaperLibrary/JRXml/XmlHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace JoyfulReaperLib.JRXml
+{
+    public static class XmlHelper
+    {
+        public static T DeserializeXml<T>(string xmlFile)
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(T));
+            FileStream fs = new FileStream(xmlFile, FileMode.Open);
+
+            var output = (T)xs.Deserialize(fs);
+            fs.Close();
+
+            return output;
+        }
+    }
+}
