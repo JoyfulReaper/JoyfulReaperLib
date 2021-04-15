9f7708ed0c98520d0b32072d5b8c05d4b3f97c49
Kyle Givler <xvegan88x@gmail.com>
2021-04-14 20:19:13 -0400

    StringHelper

---
+++ JoyfulReaperLibrary/JRText/StringHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JoyfulReaperLib.JRText
+{
+    public static class StringHelper
+    {
+        public static string AssignNullIfEmpty(string value)
+        {
+            if (String.IsNullOrWhiteSpace(value))
+            {
+                return null;
+            }
+
+            return value;
+        }
+    }
+}
