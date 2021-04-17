bae1907289d7c970a4b54208eacfc0f2e1587ae2
Kyle Givler <xvegan88x@gmail.com>
2021-04-17 16:43:20 -0400

    Todo items

--- JoyfulReaperLibrary/JRXml/Models/RSS/Rss.cs
+++ JoyfulReaperLibrary/JRXml/Models/RSS/Rss.cs
@@ -23,6 +23,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+// TODO: Correctly support tags like: cloud, image, source, enclosure, probably others
+// Docs: https://cyber.harvard.edu/rss/rss.html
 
 using System.Xml.Serialization;
 
