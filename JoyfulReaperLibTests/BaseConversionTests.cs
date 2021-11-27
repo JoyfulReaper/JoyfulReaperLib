51eb9799109c6efc4e1d7db41d254df62a75641c
Kyle Givler <xvegan88x@gmail.com>
2021-11-27 17:56:11 -0500

    Convert bin2dec dec2bin

---
+++ JoyfulReaperLibTests/BaseConversionTests.cs
@@ -0,0 +1,42 @@
+﻿using JoyfulReaperLib.JRNumbers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoyfulReaperLibTests
+{
+    [TestClass]
+    public class BaseConversionTests
+    {
+        [TestMethod]
+        public void TestDecimalToBinary()
+        {
+            Assert.AreEqual(5, BaseConverter.BinaryToDecimal("101"));
+            Assert.AreEqual(2, BaseConverter.BinaryToDecimal("10"));
+            Assert.AreEqual(1, BaseConverter.BinaryToDecimal("1"));
+            Assert.AreEqual(0, BaseConverter.BinaryToDecimal("0"));
+            Assert.AreEqual(10, BaseConverter.BinaryToDecimal("1010"));
+            Assert.AreEqual(15, BaseConverter.BinaryToDecimal("1111"));
+            Assert.AreEqual(500, BaseConverter.BinaryToDecimal("111110100"));
+            Assert.AreEqual(200, BaseConverter.BinaryToDecimal("11001000"));
+            Assert.AreEqual(11, BaseConverter.BinaryToDecimal("1011"));
+        }
+
+        [TestMethod]
+        public void TestBinaryToDecimal()
+        {
+            Assert.AreEqual("101", BaseConverter.DecimalToBinary(5));
+            Assert.AreEqual("10", BaseConverter.DecimalToBinary(2));
+            Assert.AreEqual("1", BaseConverter.DecimalToBinary(1));
+            Assert.AreEqual("0", BaseConverter.DecimalToBinary(0));
+            Assert.AreEqual("1010", BaseConverter.DecimalToBinary(10));
+            Assert.AreEqual("1111", BaseConverter.DecimalToBinary(15));
+            Assert.AreEqual("111110100", BaseConverter.DecimalToBinary(500));
+            Assert.AreEqual("11001000", BaseConverter.DecimalToBinary(200));
+            Assert.AreEqual("1011", BaseConverter.DecimalToBinary(11));
+        }
+    }
+}
---
+++ JoyfulReaperLibrary/JRNumbers/BaseConverter.cs
@@ -0,0 +1,67 @@
+﻿/*
+MIT License
+
+Copyright(c) 2020 Kyle Givler
+https://github.com/JoyfulReaper
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+using System;
+
+namespace JoyfulReaperLib.JRNumbers
+{
+    public static class BaseConverter
+    {
+        public static string DecimalToBinary(int number)
+        {
+            string result = string.Empty;
+            while (number > 0)
+            {
+                result = number % 2 + result;
+                number /= 2;
+            }
+
+            return result == "" ? "0" : result;
+        }
+
+        public static int BinaryToDecimal(string number)
+        {
+            if (!int.TryParse(number, out _))
+            {
+                throw new ArgumentException(nameof(number) + " could not be converted to an integer!");
+            }
+
+            int result = 0;
+            int place = 0;
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                var curr = int.Parse(number[i].ToString());
+                if (curr == 1)
+                {
+                    result = result + (int)Math.Pow(2, place);
+                }
+                place++;
+            }
+
+            return result;
+        }
+    }
+}
