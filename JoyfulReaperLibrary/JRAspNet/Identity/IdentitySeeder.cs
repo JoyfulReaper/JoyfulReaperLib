b14d794baa540e927fd7fbeeb2e3a7a146fea66c
Kyle Givler <xvegan88x@gmail.com>
2021-11-27 19:12:15 -0500

    Identity Seeder

---
+++ JoyfulReaperLibrary/JRAspNet/Identity/IdentitySeeder.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace JoyfulReaperLib.JRAspNet.Identity
+{
+    public static class IdentitySeeder
+    {
+        public static async void EnsurePopulated<T>(WebApplication app) where T : IdentityDbContext
+        {
+            IdentityDbContext context = app.Services
+                .CreateScope().ServiceProvider
+                .GetRequiredService<T>();
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+
+            UserManager<IdentityUser> userManager = app.Services
+                .CreateScope().ServiceProvider
+                .GetRequiredService<UserManager<IdentityUser>>();
+
+            IdentityUser user = await userManager.FindByNameAsync(app.Configuration.GetSection("IdentitySettings")["AdminUser"]);
+            if (user == null)
+            {
+                user = new IdentityUser(app.Configuration.GetSection("IdentitySettings")["AdminUser"]);
+                user.Email = app.Configuration.GetSection("IdentitySettings")["AdminUser"];
+
+                await userManager.CreateAsync(user, app.Configuration.GetSection("IdentitySettings")["AdminPassword"]);
+            }
+        }
+    }
+}
