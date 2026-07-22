3a990da50df2fce98c9c2a6cf3c5dbe67dbf3f17
Kyle Givler <6787154+JoyfulReaper@users.noreply.github.com>
2026-07-22 17:11:22 -0400

    Some renaming and Add skeleton for TcpServer library

---
+++ JoyfulReaperLib.TcpServer.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace JoyfulReaperLib.TcpServer.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ JoyfulReaperLib.TcpServer/ITcpServerOptions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JoyfulReaperLib.TcpServer;
+
+internal interface ITcpServerOptions
+{
+}
